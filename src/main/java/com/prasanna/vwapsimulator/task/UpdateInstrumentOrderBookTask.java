2b0912fe4e52ababd22dbdc57613c96d6ce043f5
itmpras <itmpras@gmail.com>
2016-02-25 18:25:46 +0000

    Updating Logs

--- src/main/java/com/prasanna/vwapsimulator/task/UpdateInstrumentOrderBookTask.java
+++ src/main/java/com/prasanna/vwapsimulator/task/UpdateInstrumentOrderBookTask.java
@@ -1,5 +1,6 @@
 package com.prasanna.vwapsimulator.task;
 
+import com.prasanna.vwapsimulator.domain.Instrument;
 import com.prasanna.vwapsimulator.domain.Tick;
 import com.prasanna.vwapsimulator.domain.TickDirection;
 import com.prasanna.vwapsimulator.orderbook.InstrumentOrderBook;
@@ -28,13 +29,14 @@ public class UpdateInstrumentOrderBookTask implements Runnable {
 
             try {
                 Tick take = instrumentOrderBook.getTicksForInstrument().take();
-                LOGGER.info("Updating {} orderbook with {}", take.getInstrument(), take);
+                Instrument instrument = take.getInstrument();
+                LOGGER.info("Updating {} orderbook with {}", instrument, take);
                 TickDirection tickDirection = take.getTickDirection();
-                LOGGER.info("Buy VWAP {}, Sell VWAP {} - Before update", instrumentOrderBook.getBuyorderVWAP(), instrumentOrderBook.getSellOrderVWAP());
+                LOGGER.info("Buy VWAP {}, Sell VWAP {} - Before update for {}", instrumentOrderBook.getBuyorderVWAP(), instrumentOrderBook.getSellOrderVWAP(),instrument);
                 // As only one thread will working to update InstrumentOrderBook wth tick , we don't have to use synchronization
                 tickDirection.updateOrderBook(instrumentOrderBook, take);
                 // Technically we can populate this information to a different queue for all sort of processings
-                LOGGER.info("Buy VWAP {}, Sell VWAP {} - After Update", instrumentOrderBook.getBuyorderVWAP(), instrumentOrderBook.getSellOrderVWAP());
+                LOGGER.info("Buy VWAP {}, Sell VWAP {} - After Update for {}", instrumentOrderBook.getBuyorderVWAP(), instrumentOrderBook.getSellOrderVWAP(),instrument);
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
             }
